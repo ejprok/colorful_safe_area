44c161a0dfc67a301c0f6d9d3c0a35dcff8befb1
Edward Prokopik <ejprok@gmail.com>
2020-04-02 20:28:17 -0700

    Able to change color of SafeArea and allow child to overflow

--- lib/colorful_safe_area.dart
+++ lib/colorful_safe_area.dart
@@ -1,7 +1,57 @@
 library colorful_safe_area;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class ColorfulSafeArea extends StatelessWidget {
+  final Widget child;
+  final Color color;
+  final bool allowOverflow;
+
+  const ColorfulSafeArea({
+    Key key,
+    @required this.child,
+    this.color = Colors.transparent,
+    this.allowOverflow = false,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData data = MediaQuery.of(context);
+    EdgeInsets padding = data.padding;
+    print(padding.top);
+
+    return Stack(
+      children: <Widget>[
+        (allowOverflow) ? child : SafeArea(child: child),
+        IgnorePointer(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: padding.top,
+                color: color,
+              ),
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      width: padding.left,
+                      color: color,
+                    ),
+                    Spacer(),
+                    Container(
+                      width: padding.right,
+                      color: color,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: padding.bottom,
+                color: color,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
 }
